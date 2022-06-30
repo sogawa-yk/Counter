b49c955b776b88fea94b7272b1589611bb04bd52
sogawa-yk <daybreaks.link@gmail.com>
2022-06-30 23:12:03 +0900

    add audioplayer, webではうまく音声再生できない

--- lib/view/muscle_count_page.dart
+++ lib/view/muscle_count_page.dart
@@ -1,9 +1,8 @@
-import 'dart:html';
-
 import 'package:counter_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:async';
+import 'package:audioplayers/audioplayers.dart';
 
 class MuscleCountPage extends ConsumerStatefulWidget {
   const MuscleCountPage({Key? key}) : super(key: key);
@@ -13,17 +12,19 @@ class MuscleCountPage extends ConsumerStatefulWidget {
 }
 
 class MuscleCountPageState extends ConsumerState<MuscleCountPage> {
-  @override
   var count = 0;
+  final _audio = AudioCache();
 
   void initState() {
     super.initState();
-    Timer.periodic(const Duration(seconds: 1), (Timer timer) {
-      count++;
-      if (count >= ref.read(NumCountProvider.state).state) {
-        timer.cancel();
-      }
+    Timer.periodic(Duration(milliseconds: 60000 ~/ ref.read(tempoProvider)),
+        (Timer timer) {
       if (mounted) {
+        count++;
+        _audio.play('audios/count_sound.wav');
+        if (count >= ref.read(NumCountProvider.state).state) {
+          timer.cancel();
+        }
         setState(
           () {},
         );
